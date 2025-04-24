15959c5a06901e98de0d845984e5179ab97a7d66
Devyan Jethwa A <vrindevj@gmail.com>
2025-04-24 23:08:27 +0300

    changes made

---
+++ Compute factorial using loop.py
@@ -0,0 +1,11 @@
+#A factorial of a number means we multiply a given number form itself down to 1
+#So in this case a loop can work with the initial value being 1
+
+def fact_checkerloop(numx):
+    answer = 1 # cant start with zero as any number multiplied by 0 is 0
+    for i in range(1,numx+1):
+        answer = answer * i
+    return answer
+
+results = int(input("Input the number you want to find the factorial on: "))
+print("Answer of factorial is: ",fact_checkerloop(results))
--- EVEN NUMBER.py
+++ EVEN NUMBER.py
@@ -2,13 +2,15 @@
 #Modulo is done to give you a remainder when you divide one number by another
 #Converts the string to a  float for computations to be performed
 
-num1 = float(input("Enter number to check if it is even or odd: "))
+def check_oddoreven(nu):
+    if nu % 2 == 0:
+        print("The number given is Even")
+    else:
+        print("The number given is odd!")
 
+numtest = int(input("Input any number to check if its odd or even: "))
+check_oddoreven(numtest)
+print()
 
-if num1%2 == 0:
-    print("The number is an even number")
-else:
-    print("The number is an odd number")
-
-#So once the you divide the number by 2 and the remainder is 0 then
+#So once the user input the number then you divide the number by 2 and the remainder is 0 then
 #it is an even number but if there is a remainder then it is an odd number
