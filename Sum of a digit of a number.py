26a816d070426b6c47c6a76ec094960929098aad
Devyan Jethwa A <vrindevj@gmail.com>
2025-04-26 15:00:50 +0300

    compute sum of digits of a number

--- Sum of a digit of a number.py
+++ Sum of a digit of a number.py
@@ -1,10 +1,11 @@
 #We can input a number convert it to a string then separate each of the string then add them by looping by converting them back to an integer
 
-def digitsummer(x)
+def digitsummer(x):
+
     sumx = 0
     for digits in str(x):
         sumx += int(digits)
-        return sumx
+    return sumx
 
 
 num1 = int(input("Input the number to find the sum : "))
